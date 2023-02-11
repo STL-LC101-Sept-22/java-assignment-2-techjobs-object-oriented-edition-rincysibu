e2e0a7e5b21a060c9cdb6b28f8d8f72af263d0f0
Rincy <rincyaslp@gmail.com>
2023-02-11 07:51:10 -0600

    pushing getters and setters

--- src/main/java/org/launchcode/techjobs/oo/CoreCompetency.java
+++ src/main/java/org/launchcode/techjobs/oo/CoreCompetency.java
@@ -18,6 +18,17 @@ public class CoreCompetency {
         this.value = value;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
     // Custom toString, equals, and hashCode methods:
 
     @Override
@@ -40,5 +51,6 @@ public class CoreCompetency {
 
     // TODO: Use the "Generate" tool to add a getter and setter for the 'value' field but
     //  ONLY a getter for the 'id' field.
+
 
 }
--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -1,7 +1,11 @@
 package org.launchcode.techjobs.oo.test;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.launchcode.techjobs.oo.Job;
+
+import static org.junit.Assert.*;
 
 /**
  * Created by LaunchCode
@@ -9,4 +13,17 @@ import org.junit.runners.JUnit4;
 @RunWith(JUnit4.class)
 public class JobTest {
 
+    @Test
+    public void testToStringStartsAndEndsWithNewLine(){
+        assertTrue(true);
+    }
+    @Test
+    public void testSettingJobId(){
+        Job job1=new Job();
+        Job job2=new Job();
+        String spec="Test Job IDs are distinct";
+        int job1Id= job1.getId();
+        int job2Id=job2.getId();
+        assertNotEquals(spec,job1Id,job2Id);
+    }
 }
